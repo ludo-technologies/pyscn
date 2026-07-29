5f7ed50c408e426be8dadb7023fea540798390e6
Hassan Raza <raihassanraza10@gmail.com>
2026-07-29 16:51:01 +0500

    test: tighten branching deadline

--- service/system_analysis_service_test.go
+++ service/system_analysis_service_test.go
@@ -39,7 +39,7 @@ func TestAnalyzeDependencies_CompletesBranchingGraphBeforeDeadline(t *testing.T)
 		paths = append(paths, path)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	response, err := NewSystemAnalysisService().AnalyzeDependencies(ctx, domain.SystemAnalysisRequest{
